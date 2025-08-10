a1a072d74c8155e89849792480825b09295c6ec3
mi11ione <i@mi11ion.io>
2025-08-10 17:38:30 +0300

    Initial Commit

---
+++ Sonar/SonarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SonarApp.swift
+//  Sonar
+//
+//  Created by mi11ion on 10/8/25.
+//
+
+import SwiftUI
+
+@main
+struct SonarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
